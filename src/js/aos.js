8c4a044466a090bfd1fe9cd0045ef47bd7679257
Oliver Millington <oli.millington@gmail.com>
2020-03-14 16:28:58 +0700

    first commit

--- src/js/aos.js
+++
@@ -1,8 +0,0 @@
-AOS.init({
-  delay: 100,
-  duration: 500,
-  once: true,
-  mirror: true,
-  once: false,
-  offset: 100,
-});
--- src/js/slider.js
+++
@@ -1,6 +0,0 @@
-$('.quote-wrapper').slick({
-  centerMode: true,
-  dots: true,
-  slidesToShow: 1,
-  centerPadding: '12rem'
-});
