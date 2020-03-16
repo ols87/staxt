527443c2de6548c8aabb5079c49abc027b2ccfe9
Oliver Millington <oli.millington@gmail.com>
2020-03-17 01:06:02 +0700

    inquirer, prompt no template

--- @staxt/page/modules/add.js
+++ @staxt/page/modules/add.js
@@ -1,7 +1,32 @@
 const fs = require('fs-extra');
+const inquirer = require('inquirer');
 
 module.exports = function () {
   this.parser('add');
+
+  const template = `${this.paths.templates}/${this.template}.hbs`;
+
+  if (!this.args.t) {
+    this.logger('blue', `No template give. using default`);
+  }
+
+  if (!fs.existsSync(template)) {
+    inquirer
+      .prompt([{
+        type: 'confirm',
+        name: 'createTemplate',
+        message: `${this.template} does not exist. Create it?`,
+        default: true,
+      }, ])
+      .then(answers => {
+        if (answers.createTemplate) {
+          fs.ensureFileSync(template);
+        } else {
+          this.template = 'default';
+          this.logger('blue', `skipping template. using default`);
+        }
+      });
+  }
 
   let data = JSON.stringify({
     template: this.template
