ad898a3c88969d68c73d06c85ac7913a9cd2034a
Oliver Millington <oli.millington@gmail.com>
2020-03-19 14:08:10 +0700

    js compiler

--- @staxt/modules/compiler/modules/js.module.js
+++ @staxt/modules/compiler/modules/js.module.js
@@ -1,8 +1,39 @@
 const fs = require('fs-extra');
 const babel = require("@babel/core");
+const minify = require("babel-minify");
 
 module.exports = function () {
-  const js = babel.transformFileSync(`${this.paths.js}/main.js`);
+  let main = fs.readFileSync(`${this.paths.js}/main.js`, 'utf8');
+  let imports = main
+    .replace(/\.js/g, "")
+    .replace(/"/g, "'")
+    .replace(/import\('/g, '')
+    .replace(/'\);/g, '')
+    .split(/[\r\n]+/);
+
+  imports.forEach((file) => {
+    if (!fs.pathExistsSync(`${this.paths.js}/${file}.js`)) {
+      this.logger('red', `Unreadable import ${file}.js`);
+      process.exit();
+    }
+
+    const contents = fs.readFileSync(`${this.paths.js}/${file}.js`, 'utf8');
+    main = main
+      .replace(/\.js/g, "")
+      .replace(`import('${file}');`, `${contents}\r\n\r\n`);
+  });
+
+  console.log(main);
+
+  fs.writeFileSync(`${this.paths.js}/bundle.js`, main);
+  const js = babel.transformFileSync(`${this.paths.js}/bundle.js`);
+
+  const {
+    code
+  } = minify(js.code);
+
   fs.ensureFileSync(`${this.paths.distAssets}/main.js`);
-  fs.writeFileSync(`${this.paths.distAssets}/main.js`, js.code);
+  fs.writeFileSync(`${this.paths.distAssets}/main.js`, code);
+
+  fs.removeSync(`${this.paths.js}/bundle.js`);
 }
