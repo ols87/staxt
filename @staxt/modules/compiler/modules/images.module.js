1242964e31ddc576348d697af45721c7b60b78a7
Oliver Millington <oli.millington@gmail.com>
2020-03-21 19:24:17 +0700

    remove old

--- @staxt/modules/compiler/modules/images.module.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs-extra');
-
-module.exports = function () {
-  fs.copySync(this.paths.images, `${this.paths.dist}/assets/images`);
-}
--- @staxt/modules/page/modules/remove.module.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs-extra');
-
-module.exports = function () {
-  this.parser();
-  const paths = this.paths;
-
-  fs.remove(`${paths.pages}/${this.page}`);
-  fs.remove(`${paths.dist}/${this.page}`);
-}
