81756ab1348d5cf315eb95289ed1ccf0aceb8dfb
Oliver Millington <oli.millington@gmail.com>
2020-03-23 11:24:05 +0700

    fix watcher bug

--- scripts/watch.js
+++ scripts/watch.js
@@ -7,51 +7,38 @@ const images = require('./images');
 
 const paths = require('../helpers/paths');
 
-const watch = {
-  init: function (server) {
-    this.server = server;
-
-    chokidar.watch(paths.src.base, {
-      ignoreInitial: true
-    }).on('all', (event, path) => {
-      if (event == 'add' || event == 'change') {
-        const isPage = path.indexOf('pages') > -1;
-        const isImg = path.indexOf('images') > -1;
-
-        let type = path.split('/');
-        type = type[type.length - 1];
-        type = type.split('.').pop();
-
-        if (isPage && type === 'js') return this.page(path);
-        if (isImg) return this.images();
-
-        this[type]();
-
-        this.server.reload();
-      }
-    });
-  },
-  page: function (path = '') {
-    path = path.replace(`${paths.src.pages}/`, '');
-    path = path.split('.')[0];
+const compiler = {
+  page: (path = '') => {
+    path = path.replace(`${paths.src.pages}/`, '').split('.')[0];
     compile(path);
   },
-  hbs: function () {
-    compile();
-  },
-  js: function () {
-    js();
-  },
-  scss: function () {
-    scss();
-  },
-  images: function () {
-    images();
-  }
+  hbs: compile,
+  js: js,
+  scss: scss,
+  images: images,
 }
 
-if (require.main === module) {
-  watch.init();
-} else {
-  module.exports = watch;
+function watch(server) {
+  chokidar.watch(paths.src.base, {
+    ignoreInitial: true
+  }).on('all', (event, path) => {
+    if (event == 'add' || event == 'change') {
+      const isPage = path.indexOf('pages') > -1;
+      const isImg = path.indexOf('images') > -1;
+
+      let type = path.split('/');
+      type = type[type.length - 1];
+      type = type.split('.').pop();
+
+      if (isPage && type === 'js') return compiler.page(path);
+
+      compiler[type]();
+
+      if (server) {
+        server.reload();
+      }
+    }
+  });
 }
+
+module.exports = watch;
