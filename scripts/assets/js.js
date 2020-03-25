0865ee05ef8d56f2c4f1d644c41ea6618ddbb591
Oliver Millington <oli.millington@gmail.com>
2020-03-25 13:52:31 +0700

    compile data checker. JS browserfy path

--- scripts/assets/js.js
+++ scripts/assets/js.js
@@ -1,24 +1,24 @@
-const fs = require('fs-extra');
+const fs = require("fs-extra");
 const browserify = require("browserify");
 
-const paths = require('../../helpers/paths');
-const timer = require('../../helpers/timer')();
-const logger = require('../../helpers/logger');
+const paths = require("../../helpers/paths");
+const timer = require("../../helpers/timer")();
+const logger = require("../../helpers/logger");
 
-module.exports = function () {
+module.exports = function() {
   const src = paths.src.assets.js;
   const dist = paths.dist.assets.js;
 
   timer.start();
 
   browserify(`${src}/main.js`)
-    .transform(`${__staxt}/node_modules/babelify`, {
+    .transform("babelify", {
       presets: ["@babel/preset-env"]
     })
     .bundle()
     .pipe(fs.createWriteStream(`${dist}/main.js`));
 
   timer.end().then(seconds => {
-    logger('green', `Javascript compiled in ${seconds} seconds`);
+    logger("green", `Javascript compiled in ${seconds} seconds`);
   });
-}
+};
--- scripts/pages/compile.js
+++ scripts/pages/compile.js
@@ -1,11 +1,11 @@
-const fs = require('fs-extra');
-const args = require('yargs').argv;
+const fs = require("fs-extra");
+const args = require("yargs").argv;
 
-const handlebars = require('../../helpers/handlebars');
-const paths = require('../../helpers/paths');
+const handlebars = require("../../helpers/handlebars");
+const paths = require("../../helpers/paths");
 const glob = require("../../helpers/glob");
-const timer = require('../../helpers/timer')();
-const logger = require('../../helpers/logger');
+const timer = require("../../helpers/timer")();
+const logger = require("../../helpers/logger");
 
 function compile(path = args.p) {
   const dataPath = `${paths.src.pages}/${path}.js`;
@@ -15,18 +15,22 @@ function compile(path = args.p) {
 
   const page = path.split("/").pop();
 
-  let outPath = page === 'index' ? '/' : path.replace(`/${page}`, '');
+  if (Object.keys(data).length < 1) {
+    logger("red", `${page} data is invalid or not exported`);
+  }
+
+  let outPath = page === "index" ? "/" : path.replace(`/${page}`, "");
 
   if (data.slug) {
-    outPath = data.slug.replace(/\/+$/, '');
+    outPath = data.slug.replace(/\/+$/, "");
   }
 
   const output = `${paths.dist.base}/${outPath}/index.html`;
 
-  const contents = fs.readFileSync(template, 'utf8');
+  const contents = fs.readFileSync(template, "utf8");
 
   if (!contents) {
-    logger('red', `${page} is referencing an inavlid or empty template`);
+    logger("red", `${page} is referencing an inavlid or empty template`);
     process.exit();
   }
 
@@ -34,17 +38,17 @@ function compile(path = args.p) {
   const html = compile(data);
   fs.outputFileSync(output, html);
 
-  delete require.cache[require.resolve(dataPath)]
+  delete require.cache[require.resolve(dataPath)];
 }
 
-module.exports = function (path = args.p) {
+module.exports = function(path = args.p) {
   if (path) {
     timer.start();
 
     compile(path);
 
     timer.end().then(seconds => {
-      logger('green', `${path} page compiled in ${seconds} seconds`);
+      logger("green", `${path} page compiled in ${seconds} seconds`);
     });
 
     return;
@@ -52,19 +56,19 @@ module.exports = function (path = args.p) {
 
   const pages = glob({
     dir: paths.src.pages,
-    includes: '.js',
-    excludes: '.data.'
+    includes: ".js",
+    excludes: ".data."
   });
 
   timer.start();
 
-  pages.forEach((page) => {
-    page = page.replace(`${paths.src.pages}/`, '');
-    page = page.replace('.js', '');
+  pages.forEach(page => {
+    page = page.replace(`${paths.src.pages}/`, "");
+    page = page.replace(".js", "");
     compile(page);
   });
 
   timer.end().then(seconds => {
-    logger('green', `All pages compiled in ${seconds} seconds`);
+    logger("green", `All pages compiled in ${seconds} seconds`);
   });
-}
+};
