f67747b6eeb5334d948ae2f1113df91053707b5a
Oliver Millington <oli.millington@gmail.com>
2020-04-04 15:14:18 +0700

    babelify

--- scripts/assets/js.js
+++ scripts/assets/js.js
@@ -1,6 +1,7 @@
 const fs = require("fs-extra");
 const args = require("yargs").argv;
 const browserify = require("browserify");
+const babelify = require("babelify");
 const concat = require("concat");
 
 const paths = require("../../helpers/paths");
@@ -8,7 +9,7 @@ const timer = require("../../helpers/timer")();
 const logger = require("../../helpers/logger");
 const config = require("../../helpers/config");
 
-module.exports = function(path = args.f) {
+module.exports = function (path = args.f) {
   const libTest = /\/?libs\//g;
   const src = paths.src.assets.js;
   const dist = paths.dist.assets.js;
@@ -19,25 +20,25 @@ module.exports = function(path = args.f) {
     (path === "libs" && config.libs.length > 0) ||
     (!path && libTest.test(path))
   ) {
-    const libs = config.libs.map(lib => {
+    const libs = config.libs.map((lib) => {
       lib = lib.replace(".js", "");
       lib = `${src}/${lib}.js`;
       return lib;
     });
     concat(libs, `${dist}/libs.js`).then(() => {
-      timer.end().then(seconds => {
+      timer.end().then((seconds) => {
         logger("green", `JS libs compiled in ${seconds} seconds`);
       });
     });
   } else {
     browserify(`${src}/main.js`)
-      .transform("babelify", {
-        presets: ["@babel/preset-env", "minify"]
+      .transform(babelify, {
+        presets: ["@babel/preset-env", "minify"],
       })
       .bundle()
       .pipe(fs.createWriteStream(`${dist}/main.js`))
       .on("finish", () => {
-        timer.end().then(seconds => {
+        timer.end().then((seconds) => {
           logger("green", `Javascript compiled in ${seconds} seconds`);
         });
       });
