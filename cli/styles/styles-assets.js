090fcc10c40d4e98903cff997e648d0e0af53831
ols87 <39114128+ols87@users.noreply.github.com>
2020-04-28 22:56:51 +0700

    Update styles-assets.js

--- cli/styles/styles-assets.js
+++ cli/styles/styles-assets.js
@@ -1,16 +1,11 @@
 const args = require('yargs').argv;
 
 const assetService = require(`${__staxt}/services/asset`);
-const styleService = require(`${__staxt}/services/styles`);
 
 module.exports = function stylesAssets(filePath = args.a, outPath = args.o || filePath) {
-  const filePaths = assetService.filePaths({
+  assetService.main({
     filePath,
     outPath,
     fileExtension: 'scss',
   });
-
-  if (!filePaths) return;
-
-  styleService(filePaths);
 };
