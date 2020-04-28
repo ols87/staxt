48d73b33cb42f09a78fe7b3f87863c4047aff5df
ols87 <39114128+ols87@users.noreply.github.com>
2020-04-28 22:40:54 +0700

    Update scripts-assets.js

--- cli/scripts/scripts-assets.js
+++ cli/scripts/scripts-assets.js
@@ -1,7 +1,6 @@
 const args = require('yargs').argv;
 
 const assetService = require(`${__staxt}/services/asset`);
-const scriptService = require(`${__staxt}/services/script`);
 
 module.exports = function scriptsAssets(filePath = args.a, outPath = args.o || filePath) {
   assetService.main({
