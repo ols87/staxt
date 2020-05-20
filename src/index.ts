43f052d003f758aa1f4d2d9f7e43035f6b05e04c
Oliver Millington <oli.millington@gmail.com>
2020-05-20 14:16:43 +0700

    state add and edit

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,7 @@ require('module-alias/register');
 import { EnvUtil, LoggerUtil, StateUtil } from '@utils';
 
 (async () => {
-  // LoggerUtil.success('Testing 123');
-  // LoggerUtil.debug('Testing 123');
   EnvUtil.get('NODE_ENV');
-  LoggerUtil.log(StateUtil.get({ keyMap: 'foo.bar.fizz', keyType: 'string' }));
+  StateUtil.add({ keyMap: 'foo.bar.fizz.buzz', keyData: 1 });
+  StateUtil.add({ keyMap: 'foo.bar', keyData: { fizz: { buzz: 1 } } });
 })();
--- src/utils/src/state-util.ts
+++ src/utils/src/state-util.ts
@@ -1,49 +1,90 @@
 import { LoggerUtil } from '../';
 
-export class StateUtil {
-  private static state: any = {
-    foo: {
-      bar: {
-        fizz: 1,
-      },
-    },
-  };
+export interface StateConfig {
+  keyMap: string;
+  keyData?: any;
+  keyType?: string;
+  merge?: boolean;
+}
 
-  public static get({ keyMap, keyType }: any): any {
-    let keyList = [];
+export interface StateResponse {
+  data: any;
+  key: string;
+  keyList: Array<string>;
+}
+
+export class StateUtil {
+  private static state: any = { test: 1 };
+
+  public static add({ keyMap, keyData }: StateConfig): any {
+    let request: StateResponse = this.request(keyMap);
+
+    if (request.data) {
+      return LoggerUtil.error(`${request.key} already exists. Use StateUtil.edit()`);
+    }
+
+    const keyList: IterableIterator<[number, string]> = request.keyList.entries();
+    const keyListLength: number = request.keyList.length;
+    let data: any = this.state;
+
+    for (let [keyIndex, keyName] of keyList) {
+      if (keyIndex === keyListLength - 1) {
+        data[keyName] = keyData;
+      } else {
+        data[keyName] = data[keyName] || {};
+      }
+
+      data = data[keyName];
+    }
+
+    return keyData;
+  }
+
+  public static get({ keyMap, keyType }: StateConfig): any {
+    let request: StateResponse = this.request(keyMap);
+
+    if (!request.data) {
+      return LoggerUtil.error(`${request.key} does not exist`);
+    }
+
+    if (keyType && typeof request.data !== keyType) {
+      LoggerUtil.warn(`typeof ${keyMap} !== '${keyType}'`);
+      LoggerUtil.warn(`typeof ${keyMap} === '${typeof keyMap}'`);
+    }
+
+    return request.data;
+  }
+
+  public static edit({ keyMap, keyType, merge }: StateConfig): any {}
+
+  public static remove({ keyMap, keyType }: StateConfig): any {}
+
+  public static clear(): any {}
+
+  public static request(keyMap: string): StateResponse {
+    let keyList: Array<string> = [];
 
     try {
       keyList = keyMap.split('.');
     } catch {
-      return LoggerUtil.error(`keymap must be a string e.g 'foo.bar' ${keyMap}`);
+      LoggerUtil.error(`keymap must be a string e.g 'foo.bar' ${keyMap}`);
     }
 
-    let stateKey = this.state;
-    let currentKey: string = '';
-    let keyIndex: number = 0;
-    let keyLength: number = keyList.length;
+    let data: any = this.state;
+    let key: string = '';
 
-    for (keyIndex; keyIndex < keyLength; keyIndex++) {
-      const key = keyList[keyIndex];
-      currentKey += `${key}.`;
-      const accessKey = currentKey.substring(0, currentKey.length - 1);
-      const has = Object.prototype.hasOwnProperty;
+    for (let [, keyName] of keyList.entries()) {
+      key += `${keyName}.`;
 
-      if (has.call(stateKey, key)) {
-        stateKey = stateKey[key];
-      } else {
-        LoggerUtil.error(`${accessKey} does not exist`);
+      try {
+        data = data[keyName];
+      } catch {
         break;
       }
     }
 
-    if (typeof keyMap == keyType) {
-      LoggerUtil.warn(`${keyMap} is not typeof ${keyType}.`);
-      LoggerUtil.debug(`typeof ${keyMap} is '${typeof keyMap}'.`);
-    }
+    key = key.replace(/.\s*$/, '');
 
-    return stateKey;
+    return { data, key, keyList };
   }
-
-  public static set(key: string): any {}
 }
